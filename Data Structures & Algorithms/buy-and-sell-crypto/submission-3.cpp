8d12f4b1dd9df88cbe2ee64af13d0d8eba2cbdfa
Andrew Ignatius <126030763+lvkji@users.noreply.github.com>
2026-04-03 21:15:24 -0500

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+        while(right != prices.size()){
+            if(prices[left] < prices[right]){
+                int profit = prices[right] - prices[left];
+                maxProfit = std::max(profit,maxProfit);
+                right++;
+            } else if(prices[left] >= prices[right]){
+                left = right;
+                right++;
+            }
+        }
+        return maxProfit;
+    }
+};
